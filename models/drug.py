c22b682f15e8e4d692a59b72b61bda4050fdd8e2
Dtikoli <tikolidavid@yahoo.com>
2023-10-24 14:28:59 -0700

    Updating drug class

--- models/drug.py
+++ models/drug.py
@@ -2,14 +2,12 @@
 """ Holds class Drug """
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Text
 
 
 class Drug(BaseModel, Base):
     """ Representation of drugs """
     __tablename__ = 'drugs'
-    principal_drug = Column(String(128), nullable=False)
-    other_drug_1 = Column(String(128))
-    other_drug_2 = Column(String(128))
+    drug = Column(Text, nullable=False)
     patient_id = Column(String(60), ForeignKey('patients.id'), nullable=False)
     case_id = Column(String(60), ForeignKey('cases.id'), nullable=False)
