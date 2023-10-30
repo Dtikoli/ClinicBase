578e25aa8b572b2773867a2cc2a9ede1fd9ef33d
Gig2341 <55427509+Gig2341@users.noreply.github.com>
2023-10-30 15:14:20 +0000

    Create clinic_app.py

---
+++ clinic_app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Flask Application """
+
+from models import storage
+from dashboards import create_app
+
+
+app = create_app()
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
